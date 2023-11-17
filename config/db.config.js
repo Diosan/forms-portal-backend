97f3ddc6f4aef0164d03e1b213afae7f04a843ab
hilwyn <hilwyn@gmail.com>
2023-11-17 14:59:39 -0500

    tested

--- config/db.config.js
+++ config/db.config.js
@@ -3,7 +3,7 @@
 module.exports = {
     HOST: "localhost",
     USER: "root",
-    PASSWORD: "piccolo",
+    PASSWORD: "Tt,7$kz,m<`9<9qs",
     DB: "jsswf_admin",
     dialect: "mysql",
     pool: {
