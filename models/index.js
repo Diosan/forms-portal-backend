4b90c1ff76521363fd3c83700f3a031ab2666a47
hilwyn <hilwyn@gmail.com>
2023-11-27 12:37:19 -0500

    master fixed

--- models/index.js
+++ models/index.js
@@ -38,7 +38,6 @@ export const ChargesModel = createChargesModel(sequelize);
 // db.accuseds = require("./accuseds.model.js")(sequelize, Sequelize);
 // db.charges = require("./charges.model.js")(sequelize, Sequelize);
 
-<<<<<<< HEAD
 // ---------------------
 // ASSOCIATIONS
 // Users ++++++++++
@@ -54,7 +53,3 @@ SubmissionModel.hasMany(UserModel)
 ChargesModel.belongsTo(AccusedModel, { foreignKey: 'accusedId', onDelete: 'RESTRICT', onUpdate: 'CASCADE'  });
 AccusedModel.hasMany(ChargesModel)
 // Charges ++++++++++
-=======
-
-module.exports = db ; 
->>>>>>> 23c84a2 (Commit)
