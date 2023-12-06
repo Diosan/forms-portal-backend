9d9c23f95ac1d783bb91fb522f972ab64a404ed4
Dion Santana <dion.santana@gmail.com>
2023-12-05 21:31:52 -0400

    Signatures class

---
+++ utilities/SIgnatures.class.js
@@ -0,0 +1,26 @@
+import { db, SubmissionModel, AccusedModel, ChargesModel, PendingModel, ConvictionModel } from "../models/index.js";
+import {dbConfig} from "../config/db.config.js";
+import mysql from 'mysql2';
+
+
+export class Signatures {
+
+    async complainantSubmissionSign(email, submission_id) {
+        let submission = await SubmissionModel.findByPk(submission_id);
+        
+    }
+
+    async commissionerSubmissionSign(email, submission_id) {
+
+    }
+
+    async verifySubmissionComplianantSignature(email, submission_id) {
+
+    }
+
+    async verifySubmissionCommissionerSignature(email, submission_id) {
+        
+    }
+
+
+}
