feafd39d95e63fc273415e549367710d6cdf32f4
hilwyn <hilwyn@gmail.com>
2023-12-12 15:40:54 -0500

    commit

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -35,7 +35,7 @@ module.exports = {
         // "post-deploy" : "npm install && sudo nginx -s reload && pm2 startOrRestart ecosystem.config.js --env production && pm2 save"
         // "post-deploy": "cd /var/www/html/jsswf-server/current && pm2 startOrRestart ecosystem.config.js --env production && pm2 save",
         // "post-deploy": "git reset --hard && git pull origin master && cd /var/www/html/jsswf-server/current && pm2 startOrRestart ecosystem.config.js --env production && pm2 save"
-        "post-deploy": "git pull origin master && cd /var/www/html/forms-poral-backend/current && pm2 startOrRestart ecosystem.config.js --env production && pm2 save"
+        "post-deploy": "pm2 startOrRestart ecosystem.config.js --env production && pm2 save"
       },
       staging: { 
         user : "root",
@@ -45,7 +45,7 @@ module.exports = {
         path : "/var/www/html/jsswf-server",
         // "post-deploy" : "echo Hh6832943!@ |  sudo -S nginx -s reload && pm2 startOrRestart ecosystem.config.js --env staging && pm2 save"
         // "post-deploy": "cd /var/www/html/vrfy-server/current && npm install && nginx -s reload && npm rebuild && pm2 startOrRestart ecosystem.config.js --env staging && pm2 save"
-        "post-deploy": "git reset --hard && git pull origin master && cd /var/www/html/jsswf-server/current && pm2 startOrRestart ecosystem.config.js --env staging && pm2 save"
+        "post-deploy": "git reset --hard && git pull origin master && cd /var/www/html/jss && pm2 startOrRestart ecosystem.config.js --env staging && pm2 save"
       },
       development: {
         
