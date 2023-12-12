abdb7e8848b171eb2ccd8d003fe758397f88aa9d
hilwyn <hilwyn@gmail.com>
2023-12-12 12:30:53 -0500

    commit

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ const ADMIN_PORT = process.env.ADMIN_PORT || 8080
   //________________________________________________
   // SYNC Database -  USE WHEN NECESSARY
   //---------------------------------------------
-  db.sequelize.sync();
+  // db.sequelize.sync();
   //----------------------------------------------------------------
 
 
