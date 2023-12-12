2b2023d391a9ac8d5b2e6bae599c5ce307f3c2df
hilwyn <hilwyn@gmail.com>
2023-12-12 18:07:27 -0500

    commit

--- controllers/pdf.controller.js
+++ controllers/pdf.controller.js
@@ -249,7 +249,7 @@ export const convertWithPuppeteer = async (req, res) => {
         "type" : 1,
         "casenotes" : "These are the case notes",
         "filingid" : "dcrim002",
-        // "filepath" : "https://link.testfile.org/PDF10MB",
+        "filepath" : "https://link.testfile.org/PDF10MB",
         // "returnurl" : "https://eservices.ttlawcourts.org/filing/dev/api/return.php",
         // "signatureobject" : "signatureobject"
     }
