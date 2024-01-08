59b3948646bbe3dbac649c07ff74991f408f8da7
hilwyn <hilwyn@gmail.com>
2024-01-08 14:00:23 -0400

    commit

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -35,7 +35,7 @@ module.exports = {
         // "post-deploy" : "npm install && sudo nginx -s reload && pm2 startOrRestart ecosystem.config.js --env production && pm2 save"
         // "post-deploy": "cd /var/www/html/jsswf-server/current && pm2 startOrRestart ecosystem.config.js --env production && pm2 save",
         // "post-deploy": "git reset --hard && git pull origin master && cd /var/www/html/jsswf-server/current && pm2 startOrRestart ecosystem.config.js --env production && pm2 save"
-        "post-deploy": "pm2 reload ecosystem.config.cjs --env production && pm2 save"
+        "post-deploy": "pm2 startOrRestart ecosystem.config.js  --env production && pm2 save"
       },
       staging: { 
         user : "root",
