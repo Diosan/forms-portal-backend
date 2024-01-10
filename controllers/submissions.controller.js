bf1853aed8b03464ec00bcfb8797b4129ff0c817
Dion Santana <DSantana@ttlawcourts.org>
2024-01-09 20:33:05 -0400

    More consent

--- controllers/submissions.controller.js
+++ controllers/submissions.controller.js
@@ -1259,6 +1259,129 @@ export const requestSignature = async (req, res) => {
 
 export const requestConsent = async (req, res) => {
 
+  let dpp_email = process.env.DPP_EMAIL;
+  let transporter = nodemailer.createTransport(mailConfig);
+
+  function getCurrentTimeInTrinidad() {
+    const options = {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true,
+      timeZone: "America/Port_of_Spain", // Time zone for Trinidad and Tobago
+    };
+
+    const trinidadTime = new Date().toLocaleTimeString("en-US", options);
+    return trinidadTime; // Returns time in Trinidad and Tobago time zone
+  }
+
+  const currentTime = getCurrentTimeInTrinidad();
+  const pathToImage = "https://www.ttlawcourts.org/images/swf-logo.png";
+
+  const resetEmailString = `
+  <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Verification Code</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                line-height: 1.6;
+                font-size:14px;
+            }
+            .container {
+                width: 80%;
+                margin: auto;
+                overflow: hidden;
+            }
+            .logo {
+                width: 100px;
+                height: 75px;
+            }
+            .code {
+                font-size: 30px;
+                color: #333;
+                letter-spacing:5px;
+                font-weight: bold;
+            }
+            .footer {
+                font-size: 12px;
+                color: #999;
+            }
+            .security-tip {
+                color: #ff0000;
+            }
+
+            .small-text {
+              font-size: 10px;
+          }
+
+          .swf-text{
+            font-size: 14px;
+          }
+
+          .swf-grey-bg{
+            background-color:#eee;
+            padding:20px;
+            color:#222;
+          }
+
+          .swf-grey-red{
+            background-color:#f8e6e0;
+            padding:20px;
+            color:#222;
+            margin-top:10px; 
+          }
+          .swf-time{
+            margin-top:10px; margin-bottom:30px
+          }
+
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <img src="${pathToImage}" alt="SWF Logo" class="logo"/>
+
+            <p class="swf-text">Hi, <b></b></p>
+            <p class="swf-text">Your consent signature has been requested on a submission in SWiF</p>
+
+            <p class="swf-text">Click <a href="${
+              process.env.APP_DOMAIN + "/consent/sign/" + req.body.submission_id
+            }">here</a> to sign submission</p>  
+        
+
+            <p class="small-text swf-time">This signature request was generated at ${currentTime}</p>
+
+            <div class="swf-grey-bg">
+            <p>Do not share this verification code with a third party or other employee. We will NEVER ask you for this code.</p>
+
+            </div>
+
+            <p class="security-tip swf-grey-red "><b>Security Tip</b><br/>
+            SWF will never send you unsolicited emails asking for confidential information, such as your Password, Verification Code, or User ID. 
+            We will never ask you to validate or restore your account access through email or pop-up windows.</p>
+            
+        </div>
+    </body>
+    </html>`;
+
+  async function sendEmail() {
+    try {
+      await transporter.sendMail({
+        from: `SWF <${SWF_EMAIL}>`,
+        to: dpp_email,
+        subject: "SWF - Consent Submission",
+        html: resetEmailString,
+      });
+      res.status(201).json({
+        outcome: "success",
+      });
+    } catch (error) {
+
+    }
+  }
+
+  sendEmail();
+
 };
 
 export const signIndictable = async (req, res) => {};
