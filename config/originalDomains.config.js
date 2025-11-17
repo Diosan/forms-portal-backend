7ca83a8ce00ed58687e68960edfcc850f1f1cc42
Dion Santana <dion.santana@gmail.com>
2025-11-17 15:42:44 -0400

    prviate complaints controller

---
+++ config/originalDomains.config.js
@@ -0,0 +1,13 @@
+
+// originaDomains.config.js
+export const originalDomains = [
+    "@ttps.gov.tt",
+    "@dpp.gov.tt",
+    "@link868.com",
+    "@ttlawcourts.org",
+    "@ird.gov.tt",
+    "@customs.gov.tt"
+
+];
+
+
